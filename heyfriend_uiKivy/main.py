4ba0593b5cb2496675b78f8976be9b07f576ec79
Ashlee Littleford <talittleford#gmail.com>
2023-12-30 22:16:52 +1030

    assets added

--- heyfriend_uiKivy/main.py
+++ heyfriend_uiKivy/main.py
@@ -1,15 +1,28 @@
-from kivy.app import App 
+##### IMPORTS #####
 
-from kivy.uix.label import Label
+from kivy.app import App 
+from kivy.uix.image import Image
+
+##### ASSETS #####
+
+logoImage = "https://github.com/ashlittleford/heyfriend_app/blob/7c96d7babc76284ac7f2bbf83c35cabf92ac265d/heyfriend_assests/hfLogo_1920x1080.svg"
+
+
+##### CODE #####
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        img = Image(source=logoImage,
+                    size_hint=(1, .5),
+                    pos_hint={'center_x':.5, 'center_y':.5})
 
-        return label
+        return img
 
+
+##### IMPLEMENTS CODE #####
+    
 if __name__ == '__main__':
     app = MainApp()
     app.run()
+
+
